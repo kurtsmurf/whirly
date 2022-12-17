c5b15fac0c578e6a90503be702e9d5c646cb8533
Chris Smith <chrisronsmith@gmail.com>
2022-12-17 13:54:34 -0500

    comment PlaybackPositionNode

--- src/PlaybackPositionNode.js
+++ src/PlaybackPositionNode.js
@@ -4,32 +4,35 @@
 // composite audio node:
 // https://github.com/GoogleChromeLabs/web-audio-samples/wiki/CompositeAudioNode
 
+// extends the interface of AudioBufferSourceNode with a `playbackPosition` property
 export class PlaybackPositionNode {
   constructor(context) {
     this.context = context;
+    
+    // initialize component audio nodes
     this._bufferSource = new AudioBufferSourceNode(context);
     this._splitter = new ChannelSplitterNode(context);
-    this._merger = new ChannelMergerNode(context);
+    this._out = new ChannelMergerNode(context);
     this._sampleHolder = new Float32Array(1);
   }
 
+  // get current progress between 0 and 1
   get playbackPosition() {
     this._analyser?.getFloatTimeDomainData(this._sampleHolder);
     return this._sampleHolder[0];
   }
 
-  get buffer() {
-    return this._buffer;
-  }
-
+  // creates an AudioBuffer with an extra `position` track
   set buffer(audioBuffer) {
-    this._buffer = audioBuffer;
+    // create a new AudioBuffer of the same length as param with one extra channel
+    // load it into the AudioBufferSourceNode
     this._bufferSource.buffer = new AudioBuffer({
       length: audioBuffer.length,
       sampleRate: audioBuffer.sampleRate,
       numberOfChannels: audioBuffer.numberOfChannels + 1,
     });
 
+    // copy data from the audioBuffer arg to our new AudioBuffer
     for (let index = 0; index < audioBuffer.numberOfChannels; index++) {
       this._bufferSource.buffer.copyToChannel(
         audioBuffer.getChannelData(index),
@@ -37,21 +40,27 @@ export class PlaybackPositionNode {
       );
     }
 
+    // fill up the position channel with numbers from 0 to 1
     for (let index = 0; index < audioBuffer.length; index++) {
       this._bufferSource.buffer.getChannelData(audioBuffer.numberOfChannels)[
         index
       ] = index / audioBuffer.length;
     }
 
+    // split the channels
     this._bufferSource.connect(this._splitter);
-
+    
+    // connect all the audio channels to the line out
     for (let index = 0; index < audioBuffer.numberOfChannels; index++) {
-      this._splitter.connect(this._merger, index, index);
+      this._splitter.connect(this._out, index, index);
     }
 
+    // connect the position channel to an analyzer so we can extract position data
     this._analyser = new AnalyserNode(this.context);
     this._splitter.connect(this._analyser, audioBuffer.numberOfChannels);
   }
+
+  // forward component node properties
 
   get loop() {
     return this._bufferSource.loop;
@@ -70,10 +79,6 @@ export class PlaybackPositionNode {
   }
 
   connect(...args) {
-    this._merger.connect(...args);
-  }
-
-  disconnect(...args) {
-    this._merger.disconnect(...args);
+    this._out.connect(...args);
   }
 }
